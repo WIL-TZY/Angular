0ae6cfbb27ae7b5c0a27577062aca96e6ca404bd
WIL-TZY <willowitzy@gmail.com>
2024-09-15 02:18:02 -0300

    import components

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    AboutComponent, 
+    ClientsComponent, 
+    FooterComponent, 
+    GalleryComponent, 
+    HeaderComponent, 
+    PricingComponent, 
+    ServicesComponent, 
+    TestimonialsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: '/public/css/style.css', // ./app.component.css
 })
