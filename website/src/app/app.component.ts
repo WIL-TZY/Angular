b515ef2ab6d4b38f3cf3ec80044baacf9954da6d
Nael <willowitzy@gmail.com>
2024-09-16 03:04:18 -0300

    routing

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,22 +11,24 @@ import { ServicesComponent } from './components/services/services.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet, 
-    AboutComponent, 
-    ClientsComponent, 
-    FooterComponent, 
-    GalleryComponent, 
-    HeaderComponent, 
-    PricingComponent, 
-    ServicesComponent, 
-    TestimonialsComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: '/public/css/style.css', // ./app.component.css
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+	RouterOutlet, 
+	RouterLink,
+	RouterLinkActive,
+	AboutComponent, 
+	ClientsComponent, 
+	FooterComponent, 
+	GalleryComponent, 
+	HeaderComponent, 
+	PricingComponent, 
+	ServicesComponent, 
+	TestimonialsComponent
+	],
+	templateUrl: './app.component.html',
+	styleUrl: '/public/css/style.css', // ./app.component.css
 })
 export class AppComponent {
-  title = 'Angular!'; 
+	title = 'Angular!'; 
 }
--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+// import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '/', component: HeaderComponent},
+    {path: 'home', component: HeaderComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'clients', component: ClientsComponent},
+    {path: 'pricing', component: PricingComponent},
+    {path: 'testimonials', component: TestimonialsComponent},
+    {path: 'gallery', component: GalleryComponent},
+    {path: 'services', component: ServicesComponent}
+];
