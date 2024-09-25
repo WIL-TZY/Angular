c7c7f0173ac43cf594dccb16ecef4817282d49d8
Nael <willowitzy@gmail.com>
2024-09-25 17:11:13 -0300

    404 page (part 1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { NotfoundComponent } from './components/notfound/notfound.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     {path: '**', component: NotfoundComponent},
