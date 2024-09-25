aa26d011e59097d1da9c6eb2152938e4c255c7ec
Nael <willowitzy@gmail.com>
2024-09-25 15:20:00 -0300

    404 page

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -7,14 +7,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { ServicesComponent } from './components/services/services.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
-    {path: '', component: HeaderComponent},
-    {path: 'home', component: HeaderComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'clients', component: ClientsComponent},
-    {path: 'pricing', component: PricingComponent},
-    {path: 'testimonials', component: TestimonialsComponent},
-    {path: 'gallery', component: GalleryComponent},
-    {path: 'services', component: ServicesComponent}
+    {title: "My Page | Home", path: '', component: HeaderComponent},
+    {title: "My Page | Home", path: 'home', component: HeaderComponent},
+    {title: "My Page | About", path: 'about', component: AboutComponent},
+    {title: "My Page | Clients", path: 'clients', component: ClientsComponent},
+    {title: "My Page | Pricing", path: 'pricing', component: PricingComponent},
+    {title: "My Page | Testimonials", path: 'testimonials', component: TestimonialsComponent},
+    {title: "My Page | Gallery", path: 'gallery', component: GalleryComponent},
+    {title: "My Page | Services", path: 'services', component: ServicesComponent},
+    {title: "My Page | 404", path: '**', component: NotfoundComponent},
 ];
