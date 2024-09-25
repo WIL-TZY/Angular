bc42994184821bf72854ac35eee14f7086631682
Nael <willowitzy@gmail.com>
2024-09-25 17:21:53 -0300

    404 page (part 2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent, NavComponent],
+	imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent, NavComponent, HomeComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {path: '**', component: NotfoundComponent},
+    {title: "Home", path: '', component: HomeComponent},
+    {title: "Home", path: 'home', component: HomeComponent},
+    {title: "404", path: '**', component: NotfoundComponent},
 ];
